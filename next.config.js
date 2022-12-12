3ab6cc1809210334f2677d6bdd7ff3a630bec870
ThanhNT26 <ngtruongthanh98@gmail.com>
2022-12-12 10:26:50 +0700

    refactor: Refactor code of project

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const path = require("path");
 
-module.exports = nextConfig
+console.log("path: ", path);
+
+module.exports = {
+  reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+};
