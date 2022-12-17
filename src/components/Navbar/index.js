8459f5b6ee983b7943945c3d089897c21639f73e
ThanhNT26 <ngtruongthanh98@gmail.com>
2022-12-17 13:21:11 +0700

    feat: Responsive for navigation bar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -4,40 +4,42 @@ import Link from "next/link";
 export default function Navbar({ current }) {
   return (
     <div className={styles.navigationBar}>
-      <h1 className={styles.brand}>Duckling</h1>
+      <div className={styles.container}>
+        <h1 className={styles.brand}>Duckling</h1>
 
-      <ul className={styles.menuList}>
-        <li className={styles.menuItem}>
-          <Link href="/" className={styles.navLink}>
-            Home
-          </Link>{" "}
-          {current === "home" ? (
-            <span className={styles.current}>current page</span>
-          ) : (
-            ""
-          )}{" "}
-        </li>
-        <li className={styles.menuItem}>
-          <Link href="/members" className={styles.navLink}>
-            Members
-          </Link>{" "}
-          {current === "members" ? (
-            <span className={styles.current}>current page</span>
-          ) : (
-            ""
-          )}{" "}
-        </li>
-        <li className={styles.menuItem}>
-          <Link href="/about" className={styles.navLink}>
-            About
-          </Link>{" "}
-          {current === "about" ? (
-            <span className={styles.current}>current page</span>
-          ) : (
-            ""
-          )}{" "}
-        </li>
-      </ul>
+        <ul className={styles.menuList}>
+          <li className={styles.menuItem}>
+            <Link href="/" className={styles.navLink}>
+              Home
+            </Link>{" "}
+            {current === "home" ? (
+              <span className={styles.current}>current page</span>
+            ) : (
+              ""
+            )}{" "}
+          </li>
+          <li className={styles.menuItem}>
+            <Link href="/members" className={styles.navLink}>
+              Members
+            </Link>{" "}
+            {current === "members" ? (
+              <span className={styles.current}>current page</span>
+            ) : (
+              ""
+            )}{" "}
+          </li>
+          <li className={styles.menuItem}>
+            <Link href="/about" className={styles.navLink}>
+              About
+            </Link>{" "}
+            {current === "about" ? (
+              <span className={styles.current}>current page</span>
+            ) : (
+              ""
+            )}{" "}
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
